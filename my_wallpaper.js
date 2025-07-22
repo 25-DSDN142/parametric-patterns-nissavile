328f5e180e39dfa31b8c232c88051b2e2f621021
nissavile <nissa.vile@icloud.com>
2025-07-22 12:01:56 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,7 +1,11 @@
 //your parameter variables go here!
-let rect_width  = 20;
-let rect_height = 30;
+let x  = 20;
+let y = 30;
 
+let midx = 100
+let midy = 100
+
+angleMode(DEGREES)
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
@@ -21,5 +25,73 @@ function wallpaper_background() {
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+  
+let green = color(188, 224, 186)
+
+  strokeWeight(9)
+  stroke(green)
+  line(midx,0,midx,midy+100)
+  line(0,midy,midx+100,midy)
+
+//
+  rotate(0)
+  line(midx-100,midy-100,midx+100,midy+100)
+  line(midx-100,midy+100,midx+100,midy-100)
+  
+  // triangle(midx-40,midy-70,midx-70,midy-70,midx-70,midy-40)
+  
+  let blue = color(139,172,224);
+  let pink = color(207, 138, 192)
+
+
+noFill()
+// triangle(midx-40,midy-70,midx-70,midy-70,midx-70,midy-40)
+
+
+push()
+let angle = 45
+translate(midx,midy)
+noFill()
+// triangle(midx-40,midy-70,midx-70,midy-70,midx-70,midy-40)
+rotate(angle+180)
+stroke(pink)
+triangle(midx-40,midy-70,midx-70,midy-70,midx-70,midy-40)
+rotate(45)
+triangle(midx-40,midy-70,midx-70,midy-70,midx-70,midy-40)
+rotate(45)
+triangle(midx-40,midy-70,midx-70,midy-70,midx-70,midy-40)
+rotate(45)
+triangle(midx-40,midy-70,midx-70,midy-70,midx-70,midy-40)
+rotate(45)
+triangle(midx-40,midy-70,midx-70,midy-70,midx-70,midy-40)
+rotate(45)
+triangle(midx-40,midy-70,midx-70,midy-70,midx-70,midy-40)
+rotate(45)
+triangle(midx-40,midy-70,midx-70,midy-70,midx-70,midy-40)
+rotate(45)
+triangle(midx-40,midy-70,midx-70,midy-70,midx-70,midy-40)
+pop()
+
+//custom shape
+
+
+stroke(blue)
+line(midx-20,midy,midx,midy-20)
+line(midx,midy-20,midx+20,midy)
+line(midx+20,midy,midx,midy+20)
+line(midx,midy+20,midx-20,midy)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
